d5fec6def03dfa3fc10e82ff7d422e36225d48d4
yopiwakame <yoshinoriyoshi7@gmail.com>
2024-10-15 08:28:57 +0900

    検索機能編集中〜

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,21 @@ class UsersController extends Controller
     public function profile(){
         return view('users.profile');
     }
-    public function search(){
-        return view('users.search');
+    
+        
+        
+    public function search(Request $request)
+    {
+        // 1つ目の処理
+        $keyword = $request->input('keyword');
+        // 2つ目の処理
+        if(!empty($keyword)){
+             $post = Post::where('post','like', '%'.$keyword.'%')->get();
+        }else{
+             $post = Post::all();
+        }
+        // 3つ目の処理
+        return redirect('/users.search',['post'=>$post]);
     }
+        
 }
