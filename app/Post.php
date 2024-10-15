bf233f6e53523a534a9e0c97642669af5a0674bf
yopiwakame <yoshinoriyoshi7@gmail.com>
2024-10-16 08:40:16 +0900

    Post.php を更新

--- app/Post.php
+++ app/Post.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model {
 
 
+     //テーブル名を指定
+    protected $table = 'posts';
+
+
+
 
     protected $fillable = ['user_id', 'post'];
+    
+    
 }
